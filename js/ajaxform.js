a3a90c762deb91c24e97f99f2224564d041e4780
Daniel Loader <spicypixel@gmail.com>
2018-04-28 21:11:56 +0100

    initial commit

---
+++ js/ajaxform.js
@@ -0,0 +1,26 @@
+var databaseDates = ["05/04/2018", "07/04/2018", "22/04/2018"];
+
+var dateExcluder = function(date) {
+    var string = jQuery.datepicker.formatDate("dd/mm/yy", date);
+    return [databaseDates.indexOf(string) >= 0];
+};
+
+var ajaxPost = function() {
+    var dateSelected = $("#date").val();
+    $.ajax({
+        url: "php/ajax.php",
+        data: { date: dateSelected },
+        type: "POST",
+        success: function(data) {
+            $("#result").html(data);
+        }
+    });
+};
+
+$(document).ready(function() {
+    $("#date").datepicker({
+        dateFormat: "dd/mm/yy",
+        beforeShowDay: dateExcluder
+    });
+    $("#submit").click(ajaxPost);
+});
